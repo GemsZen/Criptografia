ff4553f69a897d519150f12946333446478f00e6
GemsZen <GemsZen@users.noreply.github.com>
2023-04-25 23:58:52 -0300

    Initial commit

---
+++ criptografia.py
@@ -0,0 +1,113 @@
+def cripto(frase):
+    tradutor = ""
+    for letra in frase:
+        if letra in "a":
+            tradutor = tradutor + "i-=o"
+        elif letra in "b":
+            tradutor = tradutor + "6[o"
+        elif letra in "c":
+            tradutor = tradutor + "7U/"
+        elif letra in "d":
+            tradutor = tradutor + "a0[]"
+        elif letra in "e":
+            tradutor = tradutor + "49yi"
+        elif letra in "f":
+            tradutor = tradutor + "~gh6"
+        elif letra in "g":
+            tradutor = tradutor + "g7"
+        elif letra in "h":
+            tradutor = tradutor + "hgh[]"
+        elif letra in "i":
+            tradutor = tradutor + "][fh"
+        elif letra in "j":
+            tradutor = tradutor + "z;2"
+        elif letra in "k":
+            tradutor = tradutor + "~/432"
+        elif letra in "l":
+            tradutor = tradutor + "/. b"
+        elif letra in "m":
+            tradutor = tradutor + "2~2]"
+        elif letra in "n":
+            tradutor = tradutor + ";b 7"
+        elif letra in "o":
+            tradutor = tradutor + "[HdT=]"
+        elif letra in "p":
+            tradutor = tradutor + "mn-U"
+        elif letra in "q":
+            tradutor = tradutor + "0114n"
+        elif letra in "r":
+            tradutor = tradutor + "}4GDF"
+        elif letra in "s":
+            tradutor = tradutor + "hI=:"
+        elif letra in "t":
+            tradutor = tradutor + "Mw2"
+        elif letra in "u":
+            tradutor = tradutor + "ilkÇ"
+        elif letra in "v":
+            tradutor = tradutor + "h768ç"
+        elif letra in "w":
+            tradutor = tradutor + "qB ~"
+        elif letra in "x":
+            tradutor = tradutor + "MdX^-"
+        elif letra in "y":
+            tradutor = tradutor + "LR4*"
+        elif letra in "z":
+            tradutor = tradutor + "54gfn"
+        elif letra in "A":
+            tradutor = tradutor + "3xb6"
+        elif letra in "B":
+            tradutor = tradutor + "h-~cv"
+        elif letra in "C":
+            tradutor = tradutor + "v0btU"
+        elif letra in "D":
+            tradutor = tradutor + "=-b#4"
+        elif letra in "E":
+            tradutor = tradutor + "YmS0"
+        elif letra in "F":
+            tradutor = tradutor + "/g-="
+        elif letra in "G":
+            tradutor = tradutor + "*tU-"
+        elif letra in "H":
+            tradutor = tradutor + "-vnb$"
+        elif letra in "I":
+            tradutor = tradutor + "F43G"
+        elif letra in "J":
+            tradutor = tradutor + "0gfn"
+        elif letra in "K":
+            tradutor = tradutor + "}3T"
+        elif letra in "L":
+            tradutor = tradutor + "47M<"
+        elif letra in "M":
+            tradutor = tradutor + ")gç!"
+        elif letra in "N":
+            tradutor = tradutor + "=vR"
+        elif letra in "O":
+            tradutor = tradutor + "9n"
+        elif letra in "P":
+            tradutor = tradutor + "!b0"
+        elif letra in "Q":
+            tradutor = tradutor + "O0q#"
+        elif letra in "R":
+            tradutor = tradutor + "=2f"
+        elif letra in "S":
+            tradutor = tradutor + "/BT"
+        elif letra in "T":
+            tradutor = tradutor + ";b'"
+        elif letra in "U":
+            tradutor = tradutor + "+0F"
+        elif letra in "V":
+            tradutor = tradutor + "]c 1"
+        elif letra in "W":
+            tradutor = tradutor + "H07]"
+        elif letra in "X":
+            tradutor = tradutor + "n:2"
+        elif letra in "Y":
+            tradutor = tradutor + "iW4f"
+        elif letra in "Z":
+            tradutor = tradutor + "`9f3"
+        else:
+            
+            tradutor = tradutor + letra
+    return tradutor
+
+print(cripto(input("Digite sua frase para encriptação:")))            
